d9c5d9f3716d52957bf5fb71b5733f452e96608c
Simon Kraft <simon@krafit.de>
2017-03-31 11:42:56 +0200

    v.0.2

---
+++ admin.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * All the admin interface code lives in this file. Be kind!
+ *
+ * @package svelte Plugin
+ * @subpackage admin
+ * @since 0.1 - Build 1407291100 
+ */
+
+
+
+	// Init plugin options to white list our options
+		function krafit_svelte_init(){
+			register_setting( 'krafit_svelte_plugin_options', 'krafit_svelte_options', 'krafit_svelte_validate_options' );
+		}
+
+	// We want the menu to set place, right where the whole Design stuff takes place - so we hook it up in the designs menu
+		function krafit_svelte_add_options_page() {
+			add_submenu_page( 'options-general.php', __('Sharing Settings', 'svelte'), __('Sharing', 'svelte'), 'install_themes', 'sharing', 'krafit_svelte_render_form');
+		}
+
+
+	// Render the Plugin options form
+		function krafit_svelte_render_form() { ?>
+			<div class="wrap">
+				
+				<!-- Display Plugin Icon, Header, and Description -->
+				<h2><?php _e('Social Sharing Settings', 'svelte'); ?></h2>
+
+				<!-- Beginning of the Plugin Options Form -->
+				<form method="post" action="options.php">
+					<?php settings_fields('krafit_svelte_plugin_options'); ?>
+					<?php $options = get_option('krafit_svelte_options'); ?>
+
+
+					<table class="form-table">
+
+
+						<!-- Checkbox Buttons -->
+						<tr valign="top">
+							<th scope="row">Sharing-Services</th>
+							<td>
+								<!-- Facebook checkbox button -->
+								<label><input name="krafit_svelte_options[facebook]" type="checkbox" value="1" <?php if (isset($options['facebook'])) { checked('1', $options['facebook']); } ?> /> Facebook</label><br />
+
+								<!-- Twitter checkbox button -->
+								<label><input name="krafit_svelte_options[twitter]" type="checkbox" value="1" <?php if (isset($options['twitter'])) { checked('1', $options['twitter']); } ?> /> Twitter</label><br />
+
+								<!-- Google+ checkbox button -->
+								<label><input name="krafit_svelte_options[googleplus]" type="checkbox" value="1" <?php if (isset($options['googleplus'])) { checked('1', $options['googleplus']); } ?> /> Google+</label><br />
+
+								<!-- Flattr checkbox button -->
+								<label><input name="krafit_svelte_options[flattr]" type="checkbox" value="1" <?php if (isset($options['flattr'])) { checked('1', $options['flattr']); } ?> /> Flattr </label><br />
+
+								<!-- Pinterest checkbox button -->
+								<label><input name="krafit_svelte_options[pinterest]" type="checkbox" value="1" <?php if (isset($options['pinterest'])) { checked('1', $options['pinterest']); } ?> /> Pinterest </label><br />
+							
+								<!-- App.net checkbox button -->
+								<label><input name="krafit_svelte_options[appnet]" type="checkbox" value="1" <?php if (isset($options['appnet'])) { checked('1', $options['appnet']); } ?> /> App.net </label><br />
+							
+								<!-- Xing checkbox button -->
+								<label><input name="krafit_svelte_options[xing]" type="checkbox" value="1" <?php if (isset($options['xing'])) { checked('1', $options['xing']); } ?> /> Xing </label><br />
+							
+								<!-- Whatsapp checkbox button -->
+								<label><input name="krafit_svelte_options[whatsapp]" type="checkbox" value="1" <?php if (isset($options['whatsapp'])) { checked('1', $options['whatsapp']); } ?> /> Whatsapp </label>
+							
+							</td>
+						</tr>
+
+						<?php if($options['twitter'] == '1' ): ?>
+						<!-- Textbox Control -->
+						<tr>
+							<th scope="row">Sidewide Twitter name</th>
+							<td>
+								<input type="text" size="57" name="krafit_svelte_options[twitter_global]" value="<?php echo $options['twitter_global']; ?>" />
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">Personal Twitter name</th>
+							<td>
+								<label><input name="krafit_svelte_options[allow_twitter_overwrite]" type="checkbox" value="1" <?php if (isset($options['allow_twitter_overwrite'])) { checked('1', $options['allow_twitter_overwrite']); } ?> /> yes</label><br />
+								<small>Allow authors to overwrite global Twitter name with their personal name?</small>
+							</td>
+						</tr>
+						<?php endif; ?>
+
+						<?php if($options['flattr'] == '1' ): ?>
+						<!-- Textbox Control -->
+						<tr>
+							<th scope="row">Global flattr ID</th>
+							<td>
+								<input type="text" size="57" name="krafit_svelte_options[flattr_global]" value="<?php echo $options['flattr_global']; ?>" />
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">Personal flattr ID</th>
+							<td>
+								<label><input name="krafit_svelte_options[allow_flattr_overwrite]" type="checkbox" value="1" <?php if (isset($options['allow_flattr_overwrite'])) { checked('1', $options['allow_flattr_overwrite']); } ?> /> yes</label><br />
+								<small>Allow authors to overwrite global flattr ID with their personal flattr ID?</small>
+							</td>
+						</tr>
+						<?php endif; ?>
+
+					</table>
+					<p class="submit">
+					<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+					</p>
+				</form>
+
+
+
+			</div>
+		<?php	}
+
+	// Sanitize and validate input. Accepts an array, return a sanitized array.
+		function krafit_svelte_validate_options($input) {
+			 // strip html from textboxes
+			$input['flattr_global'] =  wp_filter_nohtml_kses($input['flattr_global']); // Sanitize textbox input (strip html tags, and escape characters)
+			return $input;
+		}
+
+	// Display a Settings link on the main Plugins page
+		function krafit_svelte_plugin_action_links( $links, $file ) {
+
+			if ( $file == plugin_basename( __FILE__ ) ) {
+				$posk_links = '<a href="'.get_admin_url().'options-general.php?page=sharing">'.__('Settings').'</a>';
+				// make the 'Settings' link appear first
+				array_unshift( $links, $posk_links );
+			}
+
+			return $links;
+		}
--- svelte.php
+++ svelte.php
@@ -2,46 +2,140 @@
 /*
 Plugin Name: svelte share
 Plugin URI: https://krafit.de/wordpress/plugins/svelte
-Description: 
+Description: Zus&auml;tzliche Funktionen f&uuml;r den Betrieb von lab.krafit.de.
 Author: Simon Kraft (krafit.de)
 Author URI: https://krafit.de
-Version: 0.1 - Build 1407281651
+Version: 0.2 - Build 1407291716
+Text Domain: 	'svelte'
+Domain Path: 	/lang/
 */
 
 
-function krafit_svelte_scripts() {
-	
-	wp_enqueue_style( 'svelte-style', plugins_url( 'assets/css/style.css' , __FILE__ ) );
-}
-add_action( 'wp_enqueue_scripts', 'krafit_svelte_scripts' );
+
+// ------------------ Plugin Init ------------------
+
+	// Setup Action and Filter Hooks
+		register_activation_hook(__FILE__, 'krafit_svelte_add_defaults');
+		register_uninstall_hook(__FILE__, 'krafit_svelte_delete_plugin_options');
+		add_action('admin_init', 'krafit_svelte_init' );
+		add_action('admin_menu', 'krafit_svelte_add_options_page');
+		add_filter( 'plugin_action_links', 'krafit_svelte_plugin_action_links', 10, 2 );
+
+	// Delete options table entries ONLY when plugin deactivated AND deleted
+		function krafit_svelte_delete_plugin_options() {
+			delete_option('krafit_svelte_options');
+		}
+
+	// Set textdomain
+		function krafit_svelte_lang_init() {
+		  load_plugin_textdomain( 'svelte', false, 'svelte/languages' );
+		}
+		add_action('init', 'krafit_svelte_lang_init');
+
+
+
+// ------------------ Include admin.php ------------------
+
+	// Let's include the admin.php file, if we are in dashboard
+		if ( is_admin() ) {
+			include( plugin_dir_path( __FILE__ ) . '/admin.php');
+		}
+
+
+
+// ------------------ Load styles and scripts ------------------
+
+	// Load all styles and scripts we need
+		function krafit_svelte_scripts() {
+			wp_enqueue_style( 'svelte-default-style', plugins_url( 'assets/css/style.css' , __FILE__ ) );
+		}
+		add_action( 'wp_enqueue_scripts', 'krafit_svelte_scripts' );
+
+
+
+// Hook for additional single session stuff
+	function svelte_before_sharing() {
+	    do_action('svelte_before_sharing');
+	}
+
+
+// Hook for additional single session stuff
+	function svelte_after_sharing() {
+	    do_action('svelte_after_sharing');
+	}
+
+
+
+
+
+
+// ------------------  ------------------
+
+	// Load all styles and scripts we need
 
 
 
 
 function krafit_svelte_core() {
 
+	$options = get_option( 'krafit_svelte_options' );
+	$flattrID = $options['flattr_global'];
+	$twitterID = $options['twitter_global'];
 	$svelte_link = urlencode(wp_get_shortlink());
 	$svelte_title = rawurlencode(strip_tags(get_the_title()));
 	$svelte_language = urlencode(get_bloginfo( 'language' ));
 	$svelte_language = str_replace('-','_',$svelte_language);
 
 
-		echo 'Share:  <a class="svelte facebook" href="https://www.facebook.com/sharer.php?u=' . $svelte_link . '&amp;t=' . $svelte_title . '" alt="' . __('Share on Facebook', 'krafit_svelte' ) . '" title="' . __('Share on Facebook', 'krafit_svelte' ) . '" target="_blank">' . __('Facebook', 'krafit_svelte' ) . '</a>';
-		 
-		echo ' <a class="svelte googleplus" href="https://plusone.google.com/_/+1/confirm?hl=' . $svelte_language . '&amp;url=' . $svelte_link .'" alt="' . __('Share on Google+', 'krafit_svelte' ) . '" title="' . __('Share on Google+', 'krafit_svelte' ) . '" target="_blank">' . __('Google+', 'krafit_svelte' ) . '</a>';
-		 
-		echo ' <a class="svelte twitter" href="https://twitter.com/share?text=' . $svelte_title .'%20-&amp;url=' . $svelte_link .'&amp;via=geekinheels" alt="' . __('Tweet this Post', 'krafit_svelte' ) . '" title="' . __('Tweet this Post', 'krafit_svelte' ) . '" target="_blank">' . __('Twitter', 'krafit_svelte' ) . '</a>';
-		 
-		echo ' <a class="svelte flattr" href="https://flattr.com/submit/auto?user_id=krafit&amp;url=' . $svelte_link . '&amp;title=' . $svelte_title .'&amp;language=' . $svelte_language . '&amp;&amp;category=text" alt="' . __('Support via Flattr', 'krafit_svelte' ) . '" title="' . __('Support via Flattr', 'krafit_svelte' ) . '" target="_blank">' . __('Flattr', 'krafit_svelte' ) . '</a>';
-		
-		echo ' <a class="svelte pinterest" href="http://www.pinterest.com/pin/create/button/?url=' . $svelte_link . ( (function_exists('the_post_thumbnail')) ? '&amp;media=' . wp_get_attachment_url(get_post_thumbnail_id()) : '') . '&amp;description=' . $svelte_title . '%20-%20' . $svelte_link . '" alt="' . __('Pin on Pinterest', 'krafit_svelte' ) . '" title="' . __('Pin on Pinterest', 'krafit_svelte' ) . '" target="_blank">' . __('Pinterest', 'krafit_svelte' ) . '</a>';
+		echo '<div class="svelte-social">Share:  ';
 
-		echo ' <a class="svelte appnet" href="https://alpha.app.net/intent/post?text=' . $svelte_title . '%20' . $svelte_link . '" alt="' . __('Share on App.net', 'krafit_svelte' ) . '" title="' . __('Share on App.net', 'krafit_svelte' ) . '" target="_blank">' . __('App.net', 'krafit_svelte' ) . '</a>';
-		 
-		echo ' <a class="svelte xing" href="https://www.xing-share.com/app/user?op=share;sc_p=xing-share;url=' . $svelte_link .'" alt="' . __('Share on Xing', 'krafit_svelte' ) . '" title="' . __('Share on Xing', 'krafit_svelte' ) . '" target="_blank">' . __('Xing', 'krafit_svelte' ) . '</a>';
+		svelte_before_sharing();
 
-		if (wp_is_mobile ()) {
-		echo ' <a class="svelte whatsapp" href="whatsapp://send?text=' . $svelte_title . '%20-%20' . $svelte_link .'" alt="' . __('Share on Whatsapp', 'krafit_svelte' ) . '" title="' . __('Share on Whatsapp', 'krafit_svelte' ) . '" target="_blank">' . __('Whatsapp', 'krafit_svelte' ) . '</a>';
-		}
+		if (class_exists('KrafitLikes')) { ?>
+				<span class="sociallink krafitlike"><?php krafit_likes(); ?></span>
+		<?php }
+
+		if($options['facebook'] == '1' ):
+			echo '<a class="svelte facebook" href="https://www.facebook.com/sharer.php?u=' . $svelte_link . '&amp;t=' . $svelte_title . '" alt="' . __('Share on Facebook', 'svelte' ) . '" title="' . __('Share on Facebook', 'svelte' ) . '" target="_blank">' . __('Facebook', 'svelte' ) . '</a>';
+		endif; 
+
+		if($options['twitter'] == '1' ):		 
+		echo ' <a class="svelte twitter" href="https://twitter.com/share?text=' . $svelte_title .'%20-&amp;url=' . $svelte_link .'&amp;via=krafit" alt="' . __('Tweet this Post', 'svelte' ) . '" title="' . __('Tweet this Post', 'svelte' ) . '" target="_blank">' . __('Twitter', 'svelte' ) . '</a>';
+		endif; 
+
+		if($options['googleplus'] == '1' ):
+		echo ' <a class="svelte googleplus" href="https://plusone.google.com/_/+1/confirm?hl=' . $svelte_language . '&amp;url=' . $svelte_link .'" alt="' . __('Share on Google+', 'svelte' ) . '" title="' . __('Share on Google+', 'svelte' ) . '" target="_blank">' . __('Google+', 'svelte' ) . '</a>';
+		endif; 
+
+		if($options['flattr'] == '1' ):
+		echo ' <a class="svelte flattr" href="https://flattr.com/submit/auto?user_id=' . $flattrID . '&amp;url=' . $svelte_link . '&amp;title=' . $svelte_title . '&amp;language=' . $svelte_language . '&amp;&amp;category=text" alt="' . __('Support via Flattr', 'svelte' ) . '" title="' . __('Support via Flattr', 'svelte' ) . '" target="_blank">' . __('Flattr', 'svelte' ) . '</a>';
+		endif; 
+
+		if($options['pinterest'] == '1' ):
+		echo ' <a class="svelte pinterest" href="http://www.pinterest.com/pin/create/button/?url=' . $svelte_link . ( (function_exists('the_post_thumbnail')) ? '&amp;media=' . wp_get_attachment_url(get_post_thumbnail_id()) : '') . '&amp;description=' . $svelte_title . '%20-%20' . $svelte_link . '" alt="' . __('Pin on Pinterest', 'svelte' ) . '" title="' . __('Pin on Pinterest', 'svelte' ) . '" target="_blank">' . __('Pinterest', 'svelte' ) . '</a>';
+		endif; 
+
+		if($options['appnet'] == '1' ):
+		echo ' <a class="svelte appnet" href="https://alpha.app.net/intent/post?text=' . $svelte_title . '%20' . $svelte_link . '" alt="' . __('Share on App.net', 'svelte' ) . '" title="' . __('Share on App.net', 'svelte' ) . '" target="_blank">' . __('App.net', 'svelte' ) . '</a>';
+		endif; 
+
+		if($options['xing'] == '1' ):
+		echo ' <a class="svelte xing" href="https://www.xing-share.com/app/user?op=share;sc_p=xing-share;url=' . $svelte_link .'" alt="' . __('Share on Xing', 'svelte' ) . '" title="' . __('Share on Xing', 'svelte' ) . '" target="_blank">' . __('Xing', 'svelte' ) . '</a>';
+		endif; 
+
+		if($options['whatsapp'] == '1' && wp_is_mobile ()):
+		echo ' <a class="svelte whatsapp" href="whatsapp://send?text=' . $svelte_title . '%20-%20' . $svelte_link .'" alt="' . __('Share on Whatsapp', 'svelte' ) . '" title="' . __('Share on Whatsapp', 'svelte' ) . '" target="_blank">' . __('Whatsapp', 'svelte' ) . '</a>';
+		endif; 
+
+		svelte_after_sharing();
+
+		echo '</div>';
 
 }
+
+
+
+// Hook krafit_svelte_core into out standard footer hook
+	if (has_action('entry_footer')) {
+    	add_action('entry_footer', 'krafit_svelte_core');
+	}
